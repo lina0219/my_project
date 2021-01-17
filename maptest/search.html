7b051de243b0fa97e6bea1b70df236991bcab456
lina0219 <wodms0219@gmail.com>
2021-01-17 21:03:34 +0900

    map

---
+++ maptest/search.html
@@ -0,0 +1,9 @@
+import requests
+
+searching = '합정 스타벅스'
+url = 'https://dapi.kakao.com/v2/local/search/keyword.json?query={}'.format(searching)
+headers = {
+    "Authorization": "KakaoAK ef40c66fba33237283cfb59ce3a907aa"
+}
+places = requests.get(url, headers = headers).json()['documents']
+print(places)
--- tutor/app.py
+++ tutor/app.py
@@ -3,6 +3,7 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
+
 client = MongoClient('localhost', 27017)
 db = client.travel
 
@@ -12,37 +13,30 @@ db = client.travel
 def home():
     return render_template('home.html')
 
-
 @app.route('/new_theme')
 def new_theme():
     return render_template('new_theme.html')
 
 
-@app.route('/themes', methods=['POST'])
+@app.route('/theme', methods=['POST'])
 def save_new_theme():
     name = request.form['name']
     emoji = request.form['emoji']
     spot1 = request.form['spot1']
-    spot1_lat = request.form['spot1_lat']
-    spot1_lng = request.form['spot1_lng']
     spot2 = request.form['spot2']
-    spot2_lat = request.form['spot2_lat']
-    spot2_lng = request.form['spot2_lng']
     spot3 = request.form['spot3']
-    spot3_lat = request.form['spot3_lat']
-    spot3_lng = request.form['spot3_lng']
 
-    newthemes = {'name': name, 'emoji': emoji, 'spot1': spot1, 'spot1_lat': spot1_lat, 'spot1_lng': spot1_lng, 'spot2': spot2, 'spot2_lat': spot2_lat, 'spot2_lng': spot2_lng, 'spot3': spot3, 'spot3_lat': spot3_lat, 'spot3_lng': spot3_lng}
-    db.new_theme.insert_one(newthemes)
+
+    newtheme = {'name': name, 'emoji': emoji, 'spot1': spot1, 'spot2': spot2, 'spot3': spot3}
+    db.new_theme.insert_one(newtheme)
+
     return jsonify({'result': 'success'})
 
-
 # 테마 모두 조회하는 API
-@app.route('/themes', methods=['GET'])
+@app.route('/theme', methods=['GET'])
 def read_themes():
     themes = list(db.new_theme.find({}, {'_id': 0}))
     return jsonify({'result': 'success', 'new_theme': themes})
-
 
 # 테마 하나만 조회하는 API
 @app.route('/detail', methods=['GET'])
